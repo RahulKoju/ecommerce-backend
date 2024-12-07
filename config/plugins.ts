984b99f235ed0ac7d8b424a3b03612390068ddb4
Rahulkoju <blcraz75@gmail.com>
2024-12-07 18:40:51 +0545

    strapi backend

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_API_KEY'),
+          api_secret: env('CLOUDINARY_API_SECRET'),
+        },
+      },
+    },
+  });
+  
